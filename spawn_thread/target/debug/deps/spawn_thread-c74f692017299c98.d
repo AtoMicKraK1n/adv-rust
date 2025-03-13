f896663dad3261ac20d7d81c3d25b0880f281744
Prakhar Sharma <prakharsharma2506@gmail.com>
2025-03-13 14:57:45 +0530

    Delete spawn_thread/target directory

--- spawn_thread/target/debug/deps/spawn_thread-c74f692017299c98.d
+++
@@ -1,5 +0,0 @@
-/home/krak1n/adv-rust/spawn_thread/target/debug/deps/spawn_thread-c74f692017299c98: src/main.rs
-
-/home/krak1n/adv-rust/spawn_thread/target/debug/deps/spawn_thread-c74f692017299c98.d: src/main.rs
-
-src/main.rs:
--- spawn_thread/target/debug/spawn_thread.d
+++
@@ -1,1 +0,0 @@
-/home/krak1n/adv-rust/spawn_thread/target/debug/spawn_thread: /home/krak1n/adv-rust/spawn_thread/src/main.rs
